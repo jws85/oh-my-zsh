1ce003fc154c42bebc1c69458b2eabd3c9d11f62
J. W. Smith <jwsmith2spam@gmail.com>
2011-07-15 13:21:15 -0400

    Changed git plugin to more closely match my preferences

--- plugins/git/git.plugin.zsh
+++ plugins/git/git.plugin.zsh
@@ -1,20 +1,48 @@
 # Aliases
 alias g='git'
 compdef g=git
+alias gk='gitk --all'
+alias gg='git gui'
+
+# Add
+alias ga='git add'
+compdef _git ga=git-add
+alias gai='git add --interactive'
+compdef _git gai=git-add
+alias gaa='git add -A'
+compdef _git gaa=git-add
+alias gap='git add -p'
+compdef _git gap=git-add
+
+# committing
+alias gc='git commit -v'
+compdef _git gc=git-commit
+alias gca='git commit -v -a'
+compdef _git gca=git-commit
+
+# logging
+alias gl='git log'
+compdef _git gl=git-log
+alias glg='git log --stat --max-count=5'
+compdef _git glg=git-log
+alias gla='git log --graph --decorate --all'
+compdef _git gla=git-log
+alias glol='git log --graph --decorate --pretty=oneline --abbrev-commit'
+compdef _git glol=git-log
+alias glola='git log --graph --decorate --pretty=oneline --abbrev-commit --all'
+compdef _git glola=git-log
+
+# miscellaneous
 alias gst='git status'
 compdef _git gst=git-status
-alias gl='git pull'
-compdef _git gl=git-pull
+alias gpl='git pull'
+compdef _git gpl=git-pull
 alias gup='git fetch && git rebase'
 compdef _git gup=git-fetch
 alias gp='git push'
 compdef _git gp=git-push
 gdv() { git-diff -w "$@" | view - }
 compdef _git gdv=git-diff
-alias gc='git commit -v'
-compdef _git gc=git-commit
-alias gca='git commit -v -a'
-compdef _git gca=git-commit
 alias gco='git checkout'
 compdef _git gco=git-checkout
 alias gb='git branch'
@@ -25,8 +53,6 @@ alias gcount='git shortlog -sn'
 compdef gcount=git
 alias gcp='git cherry-pick'
 compdef _git gcp=git-cherry-pick
-alias glg='git log --stat --max-count=5'
-compdef _git glg=git-log
 
 # Git and svn mix
 alias git-svn-dcommit-push='git svn dcommit && git push github master:svntrunk'
