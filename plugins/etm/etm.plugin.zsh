0199cbf6c3772ae6aacbd2221d0678a9808bdb60
J. W. Smith <jwsmith2spam@gmail.com>
2011-09-08 17:13:31 -0400

    Removed stuff from etm plugin

--- plugins/etm/etm.plugin.zsh
+++ plugins/etm/etm.plugin.zsh
@@ -1,13 +1,12 @@
 ################################################################################
 # Author: Justin Smith
-# Email: pete[dot]clark[at]gmail[dot]com
+# Email:
 # Version: 0.1 (08/03/2011)
 # License: WTFPL<http://sam.zoy.org/wtfpl/>
 #
 # oh-my-zsh plugin for etm
-#
-# (Structure of plugin taken from Pete Clark's taskwarrior plugin, a bit.)
 ################################################################################
+# TODO Completion function that works!
 
 #zstyle ':completion:*:*:task:*' verbose yes
 #zstyle ':completion:*:*:task:*:descriptions' format '%U%B%d%b%u'
@@ -17,15 +16,3 @@
 # main command
 alias e=e.py
 #compdef _etm e=etm
-
-# busy times
-alias eb='e.py b'
-#compdef _etm eb=etm
-
-# outline
-alias eo='e.py o'
-#compdef _etm eo=etm
-
-# GUI
-alias ew=e.pyw
-#compdef _etm ew=etm
